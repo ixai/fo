f769aa3d11c2c92e29b36c432f6e942274a5600a
Ixai Lanzagorta Ochoa <ixai.lanzagorta@gmail.com>
2019-07-20 16:56:39 -0500

    Setup environment
    Setup and configure base environment and development toolchain:
    - Pipenv
    - setuptools
    - .gitignore
    - flake8
    - black
    - mypy

---
+++ fo/__init__.py
@@ -0,0 +1,7 @@
+import click
+
+
+@click.command()
+def fraction_operation(operation: Operation):
+    """Perform fractions operations"""
+    pass
---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name="fo",
+    version="0.1",
+    py_modules=["fo"],
+    install_requires=["Click"],
+    entry_points="""
+        [console_scripts]
+        fo=fo:fraction_operation
+    """,
+)
