00cd69b0da38ad4502c8b198cf50f70e8561139c
Ixai Lanzagorta Ochoa <ixai.lanzagorta@gmail.com>
2019-07-21 11:43:59 -0500

    Publish package to TestPyPI via CI

--- setup.py
+++ setup.py
@@ -1,9 +1,13 @@
-from setuptools import setup
+from setuptools import find_packages, setup
 
 setup(
     name="fo",
-    version="0.1",
-    py_modules=["fo"],
+    version="0.2",
+    url="https://github.com/ixai/fo",
+    author="Ixai Lanzagorta",
+    author_email="ixai.lanzagorta@gmail.com",
+    description="Fraction Operations",
+    packages=find_packages(),
     install_requires=["Click"],
     entry_points="""
         [console_scripts]
